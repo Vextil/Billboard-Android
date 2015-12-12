cfe027a5bf4402038c9fb349ae8a3fd2e7601e28
Joaquin Cuitiño <vextil@gmail.com>
2015-12-11 21:56:01 -0300

    Welcome ProgressFragment 2.0 AKA RetrofitFragment
    RetrofitFragment manages the fragment state while waiting for a
    response, error or success. Pretty barebones right now but it removes
    most of the boilerplate on every single fragment.

--- app/src/main/kotlin/io/vextil/billboard/fragments/HomeFragment.kt
+++ app/src/main/kotlin/io/vextil/billboard/fragments/HomeFragment.kt
@@ -3,59 +3,37 @@ package io.vextil.billboard.fragments
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
+import android.view.View
 import android.widget.TextView
 
 import com.siercuit.cartelera.App
-import io.vextil.billboard.api.Home
-import com.siercuit.cartelera.interfaces.animationInterface
-import com.siercuit.cartelera.utilities.ProgressFragment
 import io.vextil.billboard.R
 import io.vextil.billboard.adapters.HomeItemAdapter
-import kotlinx.android.synthetic.main.home.*
+import io.vextil.billboard.api.Home
+import io.vextil.billboard.ui.RetrofitFragment
+import kotlinx.android.synthetic.main.home.view.*
 import rx.Observable
-import rx.Subscriber
-import rx.android.schedulers.AndroidSchedulers
-import rx.schedulers.Schedulers
 
-class HomeFragment() : ProgressFragment() {
+
+class HomeFragment() : RetrofitFragment() {
+
 
     constructor(color: Int, title: String) : this() {
-        setFragmentArguments(color, title, this)
+
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        retainInstance = true
+    override fun onGetObservable(): Observable<Home> {
+        return App.API().getHome()
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        setContentView(R.layout.home)
     }
 
-    override fun dataFetcher() {
-        var call: Observable<Home> = App.API().getHome()
-        call
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribeOn(Schedulers.computation())
-            .subscribe(object : Subscriber<Home>() {
-            override fun onCompleted() {
-            }
-
-            override fun onNext(data: Home) {
-                setData(data, Home::class.java)
-            }
-
-            override fun onError(e: Throwable) {
-                e.printStackTrace()
-            }
-        })
-    }
-
-    override fun viewBuilder() {
+    override fun onCreateSuccessView(data: Any) : View {
+        val view = inflate(R.layout.home)
         for (category in (data as Home).categories) {
-            val categoryView = LayoutInflater.from(activity).inflate(R.layout.home_category, scrollViewContainer, false)
+            val categoryView = inflate(R.layout.home_category, view.scrollViewContainer)
             val categoryName = categoryView.findViewById(R.id.header_text) as TextView
             val recyclerView = categoryView.findViewById(R.id.recycler) as RecyclerView
             categoryName.typeface = App.getRobotoTypeface()
@@ -63,18 +41,11 @@ class HomeFragment() : ProgressFragment() {
             val layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
             recyclerView.minimumHeight = 700
             recyclerView.layoutManager = layoutManager
-            val adapter = HomeItemAdapter(activity, category, (data as Home).poster)
+            val adapter = HomeItemAdapter(activity, category, data.poster)
             recyclerView.adapter = adapter
-            scrollViewContainer.addView(categoryView)
+            view.scrollViewContainer.addView(categoryView)
         }
-    }
-
-    override fun inAnimator() {
-
-    }
-
-    override fun outAnimator(callback: animationInterface) {
-
+        return view
     }
 
 }
--- app/src/main/kotlin/io/vextil/billboard/ui/MultiStateFragment.java
+++
@@ -1,7 +0,0 @@
-package io.vextil.billboard.ui;
-
-import android.support.v4.app.Fragment;
-
-public class MultiStateFragment extends Fragment {
-
-}
